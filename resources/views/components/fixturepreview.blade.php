ea496cd371e277f15daee37749724e7fe2e077fe
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-07-24 16:23:26 +0100

    created components

---
+++ resources/views/components/fixturepreview.blade.php
@@ -0,0 +1,5 @@
+<div class=" ">
+    <div class="bordertop bg-darkgrey padding text heading"> UPCOMING FIXTURES </div>
+    <div class="borderbottom  padding game text"> PREMIER LEAGUE </div>
+    
+</div>
