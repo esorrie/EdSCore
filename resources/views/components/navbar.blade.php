9c07dee7d405b24865c47595ab6e795410202564
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-07-24 16:20:06 +0100

    hover functions for main navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,20 +1,20 @@
 
-<div id="one" class="nav w-full">
+<div id="one" class="nav">
     
     <div class="menu">
         <div class="card1">
             <a href="/"><span class="text-orange">ED</span>SCORE</a>
         </div>
-        <div class="card2">
+        <div class="card2 hover:font-bold">
             <a class="{{ request()->routeIs('leagues.index') ? 'active' : '' }}" href="{{ route('leagues.index') }}">LEAGUES</a>
         </div>
-        <div class="card3">
+        <div class="card3 hover:font-bold">
             <a class="{{ request()->routeIs('teams.index') ? 'active' : '' }}" href="{{ route('teams.index') }}">TEAMS</a>
         </div>
-        <div class="card3">
+        <div class="card3 hover:font-bold">
             <a class="{{ request()->routeIs('players.index') ? 'active' : '' }}" href="{{ route('players.index') }}">PLAYERS</a>
         </div>
-        <div class="card3">
+        <div class="card3 hover:font-bold">
             <a class="{{ request()->routeIs('managers.index') ? 'active' : '' }}" href="{{ route('managers.index') }}">MANAGERS</a>
         </div>
     </div>
