c7385ed691b44b7a81d148cf641dd69ec4851fea
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-07-26 09:47:18 +0100

    added a game stats table

--- resources/views/teams/view.blade.php
+++ resources/views/teams/view.blade.php
@@ -8,7 +8,6 @@
       <div class="teamnation"> <span class="orange"> LOCATION </span> <span class="grey"> | </span> {!! $team->totalPlayers !!} PLAYERS </div>
   </div>
 </div>
-
 <div class="mb-4 bg-gradient-to-b from-fadegrey to-black border rounded-b-lg flexcenter h-14 pl-14">
   <div class="pr-5 grey hover:font-bold"> 
     <a class="{{ request()->routeIs('teams.view') ? 'active' : '' }}" href="/teams/{{ $team->id }}/{{ $team->slug }}"> OVERVIEW </a> 
@@ -21,50 +20,86 @@
   </div>
 </div>
 
-<div class="grid grid-cols-2">
-  <div class="details">
-    <div class="bordertop bg-darkgrey smallpadding text "> TEAM DETAILS</div>
-      <div class="grid grid-cols-2 grid-rows-4 details">
-        <div class="border text-sm text smallpadding"> MANAGER</div>
-        <div class="border text-sm text smallpadding uppercase text-right"> {!! $team->manager->name !!}</div>
-        <div class="border text-sm text smallpadding"> STADIUM</div>
-        <div class="border text-sm text smallpadding uppercase text-right"> {!! $team->stadium !!}</div>
-        <div class="border text-sm text smallpadding"> LOCATION</div>
-        <div class="border text-sm text smallpadding uppercase text-right"> #</div>
-        <div class="border text-sm text smallpadding"> NICKNAME</div>
-        <div class="border text-sm text smallpadding uppercase text-right"> #</div>
-      </div>
-  </div>
-  <div class=""><x-fixturepreview /></div>
-</div>
-
-<div class="details text ">
-  <div class="bordertop bg-darkgrey smallpadding text">LEAGUE</div>
-    <div class="border bg-darkgrey text grid grid-cols-10">
-      <div class="col-span-5 smallpadding"> TEAM</div>
-      <div class="self-center smallpadding"> P</div>
-      <div class="self-center smallpadding"> W</div>
-      <div class="self-center smallpadding"> D</div>
-      <div class="self-center smallpadding"> L</div>
-      <div class="self-center smallpadding"> PTS</div>
+<div class="flex justify-evenly">
+  <div class="grid grid-cols-2">
+    <div class="details">
+      <x-card.card title="Team Details">
+        {{-- <x-table.table :tableData="ADD ARRAY HERE" :headers="ADD HEADER ARRAY HERE"/> --}}
+        <div class="grid grid-cols-2 grid-rows-4 details">
+          <div class="border text-sm text smallpadding "> MANAGER</div>
+          <div class="border text-sm text smallpadding uppercase text-right "> {!! $team->manager->name !!}</div>
+          <div class="border text-sm text smallpadding "> STADIUM</div>
+          <div class="border text-sm text smallpadding uppercase text-right "> {!! $team->stadium !!}</div>
+          <div class="border text-sm text smallpadding "> CAPACITY</div>
+          <div class="border text-sm text smallpadding uppercase text-right "> {!! $team->capacity !!}</div>
+          <div class="border text-sm text smallpadding "> LOCATION</div>
+          <div class="border text-sm text smallpadding uppercase text-right "> #</div>
+          <div class="border rounded-bl-lg text-sm text smallpadding "> NICKNAME</div>
+          <div class="border rounded-br-lg text-sm text smallpadding uppercase text-right "> #</div>
+        </div>
+      </x-card.card>
+        
     </div>
-
-    <div class="uppercase text-sm">
-      <div class="grid grid-rows-5">
-        <div class="grid grid-cols-10 border">
-            @foreach ($teams as $team)
-            <div class="col-span-5 border smallpadding"> {{ $team->name }}</div>
-              <div class="border smallpadding"> #</div>
-              <div class="border smallpadding"> #</div>
-              <div class="border smallpadding"> #</div>
-              <div class="border smallpadding"> #</div>
-              <div class="border smallpadding"> #</div>
-            @endforeach
+    <div class="details text ml-16">
+        <div class="bordertop bg-darkgrey text grid grid-cols-9">
+          <div class="col-span-4 padding"> TEAM</div>
+          <div class="self-center smallpadding"> P</div>
+          <div class="self-center smallpadding"> W</div>
+          <div class="self-center smallpadding"> D</div>
+          <div class="self-center smallpadding"> L</div>
+          <div class="self-center smallpadding"> PTS</div>
+        </div>
+    
+        <div class="uppercase text-sm">
+          <div class="">
+            <div class="grid grid-cols-9 border grid-rows-5">
+              @foreach ($teams as $team)
+              <div class="col-span-4 border smallpadding hover:bg-darkgrey duration-500"> <a href="/teams/{{ $team->id }}/{{ $team->slug }}"> {!! $team->name !!} </a></div>
+                <div class="border smallpadding"> {{ $team->played }}</div>
+                <div class="border smallpadding"> {{ $team->won }}</div>
+                <div class="border smallpadding"> {{ $team->drawn }}</div>
+                <div class="border smallpadding"> {{ $team->lost }}</div>
+                <div class="border smallpadding"> {{ $team->played }}</div>
+              @endforeach
+            </div>
           </div>
         </div>
     </div>
-
+  </div>
 </div>
+
+<div class="w-full margintop">
+  <x-leaguecomponents.fixturepreview />
+</div>
+
+    <div class="details margintop w-full ">
+      <div class="bordertop bg-darkgrey smallpadding text ">GAME STATS</div>
+        <div class="grid grid-cols-17  text">
+          <div class="border bg-darkgrey smallpadding col-span-2"> DATE </div>
+          <div class="border bg-darkgrey smallpadding col-span-3"> OPP </div>
+          <div class="border bg-darkgrey smallpadding col-span-4"> RESULT </div>
+          <div class="border bg-darkgrey smallpadding"> G </div>
+          <div class="border bg-darkgrey smallpadding"> CONC </div>
+          <div class="border bg-darkgrey smallpadding"> S </div>
+          <div class="border bg-darkgrey smallpadding"> SoT </div>
+          <div class="border bg-darkgrey smallpadding"> xG </div>
+          <div class="border bg-darkgrey smallpadding"> xA </div>
+          <div class="border bg-darkgrey smallpadding"> xGA </div>
+          <div class="border bg-darkgrey smallpadding"> xAA </div>
+          <div class="text col-span-2">hgfd</div>
+          <div class="text col-span-3">hgfd</div>
+          <div class="text col-span-4">hgfd</div>
+          <div class="text"> 3</div>
+          <div class="text"> 2</div>
+          <div class="text"> 23</div>
+          <div class="text"> 5</div>
+          <div class="text"> 2.4</div>
+          <div class="text"> 1.1</div>
+          <div class="text"> 0.6</div>
+          <div class="text"> 0.7</div>
+        </div>
+    </div> 
+
 <div class="text-xs text"><a href="/teams">Go Back to teams</a></div>
 
 @endsection
