d67f794e27436ddf5cceaec37dd690616b1b39be
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-07-26 16:11:49 +0100

    .

--- resources/views/components/leaguecomponents/leaguetable.blade.php
+++ resources/views/components/leaguecomponents/leaguetable.blade.php
@@ -34,16 +34,16 @@
     </div>
 
     @foreach ($teams as $team) 
-      <div class="smallpadding border text-sm"> #</div>
-      <div class="col-span-2 smallpadding border hover:bg-orange duration-500 uppercase text-sm"> {!! $team->name !!} </div>
-      <div class="smallpadding border text-sm"> {{ $team->played }}</div>
-      <div class="smallpadding border text-sm"> {{ $team->won }} </div>
-      <div class="smallpadding border text-sm"> {{ $team->drawn }} </div>
-      <div class="smallpadding border text-sm"> {{ $team->lost }} </div>
-      <div class="smallpadding border text-sm"> {{ $team->played }} </div>
-      <div class="smallpadding border text-sm"> {{ $team->played }} </div>
-      <div class="smallpadding border text-sm"> {{ $team->played }} </div>
-      <div class="smallpadding border text-sm"> {{ $team->played }} </div>
-      <div class="col-span-2 smallpadding border hover:bg-orange duration-500 text-sm"> N/A </div>
+        <div class="smallpadding border text-sm"> #</div>      
+        <div class="col-span-2 smallpadding border hover:bg-orange duration-500 uppercase text-sm"> {!! $team->name !!} </div>
+        <div class="smallpadding border text-sm"> {{ $team->played }}</div>
+        <div class="smallpadding border text-sm"> {{ $team->won }} </div>
+        <div class="smallpadding border text-sm"> {{ $team->drawn }} </div>
+        <div class="smallpadding border text-sm"> {{ $team->lost }} </div>
+        <div class="smallpadding border text-sm"> {{ $team->played }} </div>
+        <div class="smallpadding border text-sm"> {{ $team->played }} </div>
+        <div class="smallpadding border text-sm"> {{ $team->played }} </div>
+        <div class="smallpadding border text-sm"> {{ $team->played }} </div>
+        <div class="col-span-2 smallpadding border hover:bg-orange duration-500 text-sm"> N/A </div>
     @endforeach
 </div>
