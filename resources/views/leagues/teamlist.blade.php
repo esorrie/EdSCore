405223d57183954c12841770f11852d26074d60d
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-07-26 09:37:37 +0100

    corrected teams list component

--- resources/views/leagues/teamlist.blade.php
+++ resources/views/leagues/teamlist.blade.php
@@ -24,32 +24,7 @@
   </div>
 </div>
 
-
-<div class="grid grid-cols-3">
-    <div class="bg-darkgrey border rounded-tl-lg smallpadding">
-        <div class="text"> TEAM</div>
-    </div>
-    <div class="bg-darkgrey border smallpadding">
-        <div class="text">STADIUM</div>
-    </div>
-    <div class="bg-darkgrey border rounded-tr-lg smallpadding">
-      <div class="text">CAPACITY</div>
-    </div>
-
-      @foreach ($teams as $team)
-        <div class="text-sm text smallpadding border hover:bg-darkgrey duration-500">
-          <a href="/teams/{{ $team->id }}/{{ $team->slug }}">
-            {{ $team->name }}
-          </a>
-        </div>
-        <div class="text-sm text smallpadding border hover:bg-darkgrey duration-500">
-            {{ $team->stadium}}
-        </div>
-        <div class="text-sm text smallpadding border hover:bg-darkgrey duration-500">
-            {{ $team->capacity}}
-        </div>
-    @endforeach
-</div>
+<x-leaguecomponents.teamslist :teams="$teams" />
 
 <a href="/leagues">Go Back to leagues</a>
 
