8012e3ef0708283cd9e87ff595bcee7904bd9e1b
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-07-26 09:11:59 +0100

    .

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\League;
+use App\Models\Team;
+use Illuminate\Contracts\View\View;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    // public function home(int $id): View
+    // {
+    //     $league = League::where('id', $id)->first();
+    //     $team = $league->teams;
+
+    //     return view('/', [
+    //         'teams' => $team,
+    //     ]);
+    // }
 }
