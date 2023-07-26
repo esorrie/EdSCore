ba470159456d9a5bf8ace1a6901552e6cebb26cf
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-07-26 09:36:42 +0100

    qol

--- resources/views/components/leaguecomponents/fixturepreview.blade.php
+++ resources/views/components/leaguecomponents/fixturepreview.blade.php
@@ -1,5 +1,4 @@
-<div class=" ">
-    <div class="bordertop bg-darkgrey padding text heading"> UPCOMING FIXTURES </div>
-    <div class="borderbottom  padding game text"> PREMIER LEAGUE </div>
-    
-</div>
+
+    <div class="bordertop bg-darkgrey padding text"> UPCOMING FIXTURES </div>
+    <div class="borderbottom padding text"> PREMIER LEAGUE </div>
+
