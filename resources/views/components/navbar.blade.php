d9fd1f09c22c5d869fb10300e2681681505896b3
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-07-27 17:01:45 +0100

    added login and register options to the navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -14,11 +14,20 @@
         <div class="card3 hover:font-bold">
             <a class="{{ request()->routeIs('players.index') ? 'active' : '' }}" href="{{ route('players.index') }}">PLAYERS</a>
         </div>
-        <div class="card3 hover:font-bold">
+        <div class="card3 hover:font-bold mr-96">
             <a class="{{ request()->routeIs('managers.index') ? 'active' : '' }}" href="{{ route('managers.index') }}">MANAGERS</a>
         </div>
     </div>
     
+    <div class="menu">
+        <div class="card4 hover:font-bold uppercase text-right">
+            <a class="{{ request()->routeIs('users.register') ? 'active' : '' }}" href="{{ route('users.register') }}"> Register</a>
+        </div>
+        <div class="card4 hover:font-bold uppercase">
+            <a class="{{ request()->routeIs('users.login') ? 'active' : '' }}" href="{{ route('users.login') }}"> Login</a>
+        </div>
+    </div>
+        
     <div class="card_searchbar">
         <input type="text" placeholder="Search...">
     </div>
