801decd869f8927374997471189058fa8d7bb7c8
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-08-02 15:08:26 +0100

    adjusted info displayed in team index table

--- resources/views/teams/index.blade.php
+++ resources/views/teams/index.blade.php
@@ -4,38 +4,30 @@
 
 <x-teamcomponents.teamnav />
 
-<div class="grid grid-cols-3 text margintop uppercase">
-  <div class="w-full bg-darkgrey border rounded-tl-lg"> 
-    <p class="text pl-4 uppercase">Team</p> 
-  </div>
-  <div class="w-full bg-darkgrey border"> 
-    <p class="text pl-4 uppercase">Stadium</p> 
-  </div>
-  <div class="bg-darkgrey border rounded-tr-lg">
-    <p class="text pl-4 uppercase">League</p>
-  </div>
-  
-  @foreach ($teams as $team)
-  <div class="text-sm">
-    <div class="smallpadding border hover:bg-orange duration-500 hover:font-bold">
-      <a href="/teams/{{ $team->id }}/{{ $team->slug }}">
-      <div class="w-fit"> <img style="width:50px;height:50px;" src="{{ $team->crest }}"> </div>
-        {!! $team->name !!}
-      </a>
+  <div class="grid grid-cols-2 text margintop uppercase">
+    <div class="w-full bg-darkgrey border rounded-tl-lg"> 
+      <p class="text pl-4 uppercase">Team</p> 
     </div>
-  </div>
-  <div class="smallpadding border hover:bg-darkgrey duration-500 text-sm orange"> {!! $team->stadium !!} </div>
-  <div class="smallpadding border hover:bg-darkgrey duration-500 text-sm hover:font-bold"> 
+    <div class="w-full bg-darkgrey border"> 
+      <p class="text pl-4 uppercase">Stadium</p> 
+    </div>
     
-
-    
+    @foreach ($teams as $team)
+    <div class="text-sm">
+      <div class="smallpadding border hover:bg-orange duration-500 hover:font-bold">
+        <a href="/teams/{{ $team->id }}/{{ $team->slug }}">
+        <div class="w-fit"> <img style="width:50px;height:50px;" src="{{ $team->crest }}"> </div>
+          {!! $team->name !!}
+        </a>
+      </div>
+    </div>
+    <div class="smallpadding border hover:bg-darkgrey duration-500 text-sm orange"> {!! $team->stadium !!} </div>
+    @endforeach
   </div>
-  @endforeach
-</div>
-  
-<div class="pagination text">
-  {{ $teams->links() }}
-</div>
-<div class="text-xs text"><a href="/">Go Back Home</a></div>
+    
+  <div class="pagination text">
+    {{ $teams->links() }}
+  </div>
+  <div class="text-xs text"><a href="/">Go Back Home</a></div>
 
 @endsection
