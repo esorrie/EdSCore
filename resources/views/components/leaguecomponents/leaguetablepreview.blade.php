a6ee48eb03a09912d5b82bfd186058e4a51509f2
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-08-03 09:42:46 +0100

    changed directory

--- resources/views/components/leaguecomponents/leaguetablepreview.blade.php
+++ resources/views/components/leaguecomponents/leaguetablepreview.blade.php
@@ -12,13 +12,13 @@
     <div class="uppercase text-sm">
       <div class="">
         <div class="grid grid-cols-9 border grid-rows-5">
-            @foreach ($teams as $team)
-            <div class="col-span-4 border smallpadding hover:bg-orange duration-500"> <a href="/teams/{{ $team->id }}/{{ $team->slug }}"> {!! $team->name !!} </a></div>
+          @foreach ($teams as $team)
+            <div class="col-span-4 border smallpadding hover:bg-orange duration-500"> <a href="/teams/{{ $team->id }}/{{ $team->slug }}"> {{ $team->name }} </a></div>
               <div class="border smallpadding"> {{ $team->played }}</div>
               <div class="border smallpadding"> {{ $team->won }}</div>
               <div class="border smallpadding"> {{ $team->drawn }}</div>
               <div class="border smallpadding"> {{ $team->lost }}</div>
-              <div class="border smallpadding"> {{ $team->played }}</div>
+              <div class="border smallpadding"> {{ $team->points }}</div>
             @endforeach
         </div>
       </div>
