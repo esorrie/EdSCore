bde9b1037d4c338567492fbd719e1c40c62c15a1
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-08-03 09:47:23 +0100

    adjusted column titles

--- resources/views/components/leaguecomponents/fixtures.blade.php
+++ resources/views/components/leaguecomponents/fixtures.blade.php
@@ -1,16 +1,16 @@
-<div class="text font-bold text-4xl">FIXTURES</div>
+<div class="text font-bold text-4xl uppercase">fixtures</div>
 
 <div class="margintop text">
-    <div class="grid grid-cols-6 border ">
-        <div class="border smallpadding bg-darkgrey"> DATE </div>
-        <div class="border smallpadding bg-darkgrey col-span-3"> MATCH </div>
-        <div class="border smallpadding bg-darkgrey col-span-2"> LEAGUE </div>
+    <div class="grid grid-cols-6 border  uppercase">
+        <div class="border smallpadding bg-darkgrey"> date </div>
+        <div class="border smallpadding bg-darkgrey col-span-3"> match </div>
+        <div class="border smallpadding bg-darkgrey"> score  </div>
+        <div class="border smallpadding bg-darkgrey"> referee  </div>
         
-        <div class="border smallpadding">hgfd</div>
-        <div class="col-span-3 border smallpadding">hgfds</div>
-        <div class="col-span-2 border smallpadding">hgfds</div>
+        <div class="border smallpadding orange">12/07</div>
+        <div class="col-span-3 border smallpadding"> (home team) vs (away team)</div>
+        <div class="border smallpadding"> 0 - 0</div>
+        <div class="border smallpadding orange"> mike dean </div>
         
     </div>
 </div>
-
-{{-- TODO: add this component to leagues.fixtures --}}
