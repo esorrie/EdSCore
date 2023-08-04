6479038f2616471d33d2d373f6719e938517e090
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-08-04 13:28:36 +0100

    encrypt password

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,8 +38,8 @@ class User extends Authenticatable
      *
      * @var array<string, string>
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    public function setPasswordAttribute($password)
+    {
+        $this->attriburters['password'] = bcrypt($password);
+    }
 }
