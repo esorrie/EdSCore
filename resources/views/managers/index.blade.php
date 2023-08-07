f442758e385e8e1f2c8bf985b84b42ae30d50c97
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-08-07 11:12:45 +0100

    adjusted table columns

--- resources/views/managers/index.blade.php
+++ resources/views/managers/index.blade.php
@@ -4,15 +4,12 @@
 
 <x-managercomponents.managernav />
 
-<div class="grid grid-cols-3 margintop">
+<div class="grid grid-cols-2 margintop">
     <div class="bg-darkgrey border rounded-tl-lg smallpadding">
         <div class="text"> NAME</div>
     </div>
     <div class="bg-darkgrey border smallpadding">
         <div class="text"> TEAM</div>
-    </div>
-    <div class="bg-darkgrey border rounded-tr-lg smallpadding">
-        <div class="text"> LEAGUE</div>
     </div>
 
 @foreach ($managers as $manager)
@@ -21,10 +18,7 @@
     </div>
     <div class="text-sm orange uppercase smallpadding border hover:bg-darkgrey duration-500 hover:font-bold">
         <a href="/teams/{{ $manager->team->id }}/{{ $manager->team->slug }}"> {!! $manager->team->name !!} </a>
-    </div> 
-    <div class="text-sm text uppercase smallpadding border hover:bg-darkgrey duration-500 hover:font-bold">
-        {{-- <a href="/leagues/{{ $manager->team->id }}/{{ $manager->team->slug }}"> {!! $manager->team->league->name !!} </a> --}}
-    </div> 
+    </div>  
 @endforeach
 
 {{-- <div class="pagination text">
