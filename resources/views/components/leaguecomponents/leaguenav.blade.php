6a9ee5b8be136a6f503fbe110a994ee7e55f5f5d
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-08-07 15:45:49 +0100

    styles

--- resources/views/components/leaguecomponents/leaguenav.blade.php
+++ resources/views/components/leaguecomponents/leaguenav.blade.php
@@ -1,7 +1,7 @@
 <div class="leaguenav">
     <div class="leaguesdisplay"> LEAGUES</div>
       <div class="card_searchbar">
-            <input type="text" placeholder="Search...">
+        <input type="text" class="border bg-grey border-grey text-sm rounded-lg w-full" placeholder="Search...">
       </div>
   </div>
   
--- resources/views/components/managercomponents/managernav.blade.php
+++ resources/views/components/managercomponents/managernav.blade.php
@@ -1,6 +1,6 @@
 <div class="managernav">
     <div class="managersdisplay"> MANAGERS</div>
         <div class="card_searchbar">
-            <input type="text" placeholder="Search...">
+            <input type="text" class="border bg-grey border-grey text-sm rounded-lg w-full" placeholder="Search...">
         </div>
   </div>
