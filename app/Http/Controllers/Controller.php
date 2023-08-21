446f16738bd9e291e2c591b5853f006d60fe31d3
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-08-21 15:51:24 +0100

    .

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -23,14 +23,13 @@ class Controller extends BaseController
         // $fixture = $league->fixtures->where('full_time_home', null)->first();
         // dd($fixture);
         
-        $response = Http::withHeader('X-Auth-Token', 'b7173c63c2084739b77c6fe4cb8bf7f0')->get('https://api.football-data.org/v4/teams/67/matches');
+        $response = Http::withHeader('X-Auth-Token', 'b7173c63c2084739b77c6fe4cb8bf7f0')->get('https://api.football-data.org/v4/teams/328/matches');
         dd($response->json());
     }
 
     public function home() {
 
         $fixture = Fixture::all()->where('full_time_home', null)->first();
-
         return view('home', [
             'fixture' => $fixture,
         ]);
