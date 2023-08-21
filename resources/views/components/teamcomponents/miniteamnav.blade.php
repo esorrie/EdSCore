aa0ebe5c25a44720880ade690b112a8f17c4c242
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-08-21 10:33:23 +0100

    changed nav to component

--- resources/views/components/teamcomponents/miniteamnav.blade.php
+++ resources/views/components/teamcomponents/miniteamnav.blade.php
@@ -1,15 +1,15 @@
-<div class="mb-4 bg-gradient-to-b from-fadegrey to-black border rounded-b-lg flexcenter h-14 pl-14">
+<div class="mb-4 bg-gradient-to-b from-fadegrey to-black border rounded-b-lg flexcenter h-14 pl-14 uppercase">
     <div class="pr-5 grey hover:font-bold"> 
-      <a class="{{ request()->routeIs('teams.view') ? 'active' : '' }}" href="/teams/{{ $team->id }}/{{ $team->slug }}"> OVERVIEW </a> 
+      <a class="{{ request()->routeIs('teams.view') ? 'active' : '' }}" href="/teams/{{ $team->id }}/{{ $team->slug }}"> overview </a> 
     </div>
     <div class="pr-5 grey hover:font-bold"> 
-      <a class="{{ request()->routeIs('teams.fixtures') ? 'active' : '' }}" href="/teams/{{ $team->id }}/{{ $team->slug }}/fixtures"> FIXTURES</a> 
+      <a class="{{ request()->routeIs('teams.fixtures') ? 'active' : '' }}" href="/teams/{{ $team->id }}/{{ $team->slug }}/fixtures"> fixtures</a> 
     </div>
     <div class="pr-5 grey hover:font-bold"> 
-      <a class="{{ request()->routeIs('teams.results') ? 'active' : '' }}" href="/teams/{{ $team->id }}/{{ $team->slug }}/results"> RESULTS</a> 
+      <a class="{{ request()->routeIs('teams.results') ? 'active' : '' }}" href="/teams/{{ $team->id }}/{{ $team->slug }}/results"> results</a> 
     </div>
     <div class="pr-5 grey hover:font-bold"> 
-      <a class="{{ request()->routeIs('teams.players') ? 'active' : '' }}" href="/teams/{{ $team->id }}/{{ $team->slug }}/players"> PLAYERS</a> 
+      <a class="{{ request()->routeIs('teams.players') ? 'active' : '' }}" href="/teams/{{ $team->id }}/{{ $team->slug }}/players"> players</a> 
     </div>
   </div>
   
