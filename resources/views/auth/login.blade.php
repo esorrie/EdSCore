99ed82d5faeaeb6a7234dd73c221b37d733b98a2
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2023-08-31 11:17:52 +0100

    created a register link on login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,6 +53,13 @@
                             </a>
                         @endif
                     </div>
+                    <div class="block">
+                        @if (Route::has('register'))
+                            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800 " href="{{ route('register') }}">
+                                {{ __('Don\'t have an account yet? ') }}
+                            </a>
+                        @endif
+                    </div>
                 </form>
         </div>
     </div>
