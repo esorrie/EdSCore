57af1b415dd13dbc9da7399049f624033854f64e
Ed Sorrie <ed_sorrie@hotmail.co.uk>
2024-11-27 14:18:23 +0000

    adjusted positions labels to match database positions

--- resources/views/teams/players.blade.php
+++ resources/views/teams/players.blade.php
@@ -14,17 +14,34 @@
 
 <div class="margintop">
   <x-card.card title="Defenders">
-    <x-playercomponents.playerlist :players="$players->where('position', 'Defence')" />
+    <x-playercomponents.playerlist :players="$players->whereIn('position', 
+    ['Defence',
+    'Centre-Back',
+    'Left-Back',
+    'Right-Back',
+    ])" />
   </x-card.card>
 </div>
 <div class="margintop">
   <x-card.card title="Midfielders">
-    <x-playercomponents.playerlist :players="$players->where('position', 'Midfield')" />
+    <x-playercomponents.playerlist :players="$players->whereIn('position', 
+    ['Midfield',
+    'Attacking Midfield',
+    'Central Midfield',
+    'Defensive Midfield',
+    'Left Midfield',
+    'Right Midfield'
+    ])" />
   </x-card.card>
 </div>
 <div class="margintop">
   <x-card.card title="Attackers">
-    <x-playercomponents.playerlist :players="$players->where('position', 'Offence')" />
+    <x-playercomponents.playerlist :players="$players->whereIn('position', 
+    ['Offence',
+    'Centre-Forward',
+    'Left Winger',
+    'Right Winger'
+    ])" />
   </x-card.card>
 </div>
 @endsection
